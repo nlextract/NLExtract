e28f0f48a69986eace0363b298f9feb06adcaee4
Just van den Broecke <just@justobjects.nl>
2012-01-24 00:25:48 +0100

    fix voor isue #16 mogelijkheid user/pw van settings.sh doorgeven + check of psql prog in PATH staat

--- top10nl/bin/top10-drop-tables.sh
+++ top10nl/bin/top10-drop-tables.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 #
-# clean existing tables
-# Author: Just van den Broecke
+# Util: clean existing tables
+#
+# Auteur: Just van den Broecke
+#
+
+hash psql 2>&- || { echo >&2 "Kan het PostgreSQL client programma psql niet vinden. Heb ik echt nodig..."; exit 1; }
 
 BASEDIR=`dirname $0`/..
 BASEDIR=`(cd "$BASEDIR"; pwd)`
 
 . $BASEDIR/bin/top10-settings.sh
-psql -d $PG_DB -f $BASEDIR/bin/top10-drop-tables.sql
+
+export PGPASSWORD=$PG_PASSWORD
+psql -d $PG_DB -U $PG_USER -p $PG_PORT -h $PG_HOST -f $BASEDIR/bin/top10-drop-tables.sql
 
