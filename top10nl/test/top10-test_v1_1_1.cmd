f15ca3e4c45c68cef0c78d6e58b1075f2e696e7f
Frank Steggink <frank@steggink.org>
2012-10-07 18:19:31 +0200

    Fix #76: testscripts toegevoegd (bash-script niet getest)

---
+++ top10nl/test/top10-test_v1_1_1.cmd
@@ -0,0 +1,16 @@
+:: Auteur: Just van den Broecke
+:: Test script
+
+:: Geport door Frank Steggink naar Windows CMD
+
+set TOP10NL_HOME=%~dp0..
+
+set TOP10NL_BIN=%TOP10NL_HOME%\bin
+set TOP10NL_TEST_DATA=%TOP10NL_HOME%\test\data
+set TOP10NL_TEST_TMP=%TOP10NL_HOME%\test\tmp
+
+:: Temp dir voor gesplitste GML files and .gfs bestanden
+rmdir /s /q %TOP10NL_TEST_TMP%
+mkdir %TOP10NL_TEST_TMP%
+
+python %TOP10NL_BIN%\top10-extract.py %TOP10NL_TEST_DATA%\test_v1_1_1.gml --dir %TOP10NL_TEST_TMP%
---
+++ top10nl/test/top10-test_v1_1_1.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#
+# Auteur: Just van den Broecke
+# Test script
+#
+TOP10NL_HOME=`dirname $0`/..
+TOP10NL_HOME=`(cd "$TOP10NL_HOME"; pwd)`
+TOP10NL_BIN=$TOP10NL_HOME/bin
+TOP10NL_TEST_DATA=$TOP10NL_HOME/test/data
+TOP10NL_TEST_TMP=$TOP10NL_HOME/test/tmp
+
+# Temp dir voor gesplitste GML files and .gfs bestanden
+/bin/rm -rf $TOP10NL_TEST_TMP
+mkdir $TOP10NL_TEST_TMP
+
+python $TOP10NL_BIN/top10-extract.py $TOP10NL_TEST_DATA/test_v1_1_1.gml --dir $TOP10NL_TEST_TMP
+
