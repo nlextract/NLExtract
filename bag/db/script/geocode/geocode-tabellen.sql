886c48b89eae95fef1a64fbdb338123c6718c691
Django Eijgensteijn <djangoboy@gmail.com>
2012-11-23 10:31:44 +0100

    Temporary fix for PostGIS 2.0.x Deleted references to probe_geometry_columns()

--- bag/db/script/geocode/geocode-tabellen.sql
+++ bag/db/script/geocode/geocode-tabellen.sql
@@ -385,6 +385,3 @@ FROM
 
 CREATE INDEX geo_provincie_geopunt_idx ON geo_provincie USING gist (geopunt);
 
--- Vult de geometry_columns alleen bij PostGIS 1.x versies (dus niet in 2.x+)
-select case when cast(substring(postgis_lib_version()  from 1 for 1) as numeric) < 2 then probe_geometry_columns() end;
-
