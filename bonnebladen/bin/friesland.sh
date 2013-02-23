846be5c368cf654b65fc36402c8b43cafad3e891
Just van den Broecke <just@justobjects.nl>
2013-02-23 16:47:09 +0100

    Bonne friesland, nogmaals, nu goed

--- bonnebladen/bin/friesland.sh
+++ bonnebladen/bin/friesland.sh
@@ -5,6 +5,8 @@
 ./bonnetrans.sh b091-1932.png
 ./bonnetrans.sh b092-1928.png
 
+SETTINGS_SCRIPT="settings.sh"
+. $SETTINGS_SCRIPT
 mv ${BONNE_DATA_HOME}/b074-1931.tif ${BONNE_DATA_HOME}/friesland-trans
 mv ${BONNE_DATA_HOME}/b075-1930.tif ${BONNE_DATA_HOME}/friesland-trans
 mv ${BONNE_DATA_HOME}/b091-1932.tif ${BONNE_DATA_HOME}/friesland-trans
