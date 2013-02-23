4e34b3eba04449b68446eaf241170124d028598d
Just van den Broecke <just@justobjects.nl>
2013-02-23 16:44:24 +0100

    foutje png->tif

--- bonnebladen/bin/friesland.sh
+++ bonnebladen/bin/friesland.sh
@@ -5,9 +5,9 @@
 ./bonnetrans.sh b091-1932.png
 ./bonnetrans.sh b092-1928.png
 
-mv ${BONNE_DATA_HOME}/b074-1931.png ${BONNE_DATA_HOME}/friesland-trans
-mv ${BONNE_DATA_HOME}/b075-1930.png ${BONNE_DATA_HOME}/friesland-trans
-mv ${BONNE_DATA_HOME}/b091-1932.png ${BONNE_DATA_HOME}/friesland-trans
-mv ${BONNE_DATA_HOME}/b092-1928.png ${BONNE_DATA_HOME}/friesland-trans
+mv ${BONNE_DATA_HOME}/b074-1931.tif ${BONNE_DATA_HOME}/friesland-trans
+mv ${BONNE_DATA_HOME}/b075-1930.tif ${BONNE_DATA_HOME}/friesland-trans
+mv ${BONNE_DATA_HOME}/b091-1932.tif ${BONNE_DATA_HOME}/friesland-trans
+mv ${BONNE_DATA_HOME}/b092-1928.tif ${BONNE_DATA_HOME}/friesland-trans
 
 
