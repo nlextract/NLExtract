31a845d0b06c20d6526e6d74a2e5059a8e1db748
Just van den Broecke <just@justobjects.nl>
2014-01-20 17:45:45 +0100

    refine OpenTopo TIFF script

--- opentopo/bin/topotrans.sh
+++ opentopo/bin/topotrans.sh
@@ -14,7 +14,6 @@ dst_dir=$2
 
 for src_tif in `ls -1 $src_dir/*.tif`
 do
-    # Doel tif moet in bron dir aangemaakt aanvakelijk, anders foutmelding
     dst_tif=$src_dir/p`basename $src_tif|cut -d'.' -f1`.tif
     echo $src_tif to $dst_tif
 
@@ -28,25 +27,26 @@ do
     # gdal_translate -of GTiff -co TILED=YES -co BIGTIFF=YES -co COMPRESS=JPEG -co JPEG_QUALITY=80
     #    -co BLOCKXSIZE=512 -co BLOCKYSIZE=512 -co PHOTOMETRIC=YCBCR %THIS_DIR%.vrt %THIS_DIR%.tif
 
-    # OUD: Tiff (wordt 427MB per file!)
+    # OUD: Tiff (wordt 427MB per file!) 
     # gdal_translate -of GTiff -co TILED=YES -co BLOCKXSIZE=512 -co BLOCKYSIZE=512 -a_srs EPSG:28992  $src_tif $dst_tif
 
+    # gdal_translate -b 1 -b 2 -b 3 -of GTiff -co TILED=YES -co COMPRESS=NONE -co alpha=yes -co PHOTOMETRIC=RGB -co BLOCKXSIZE=512 -co BLOCKYSIZE=512 -a_srs EPSG:28992  $src_tif $dst_tif
+
     # NIEUW met JPEG compressie en alleen 1e 3 banden meenemen....
-    gdal_translate -b 1 -b 2 -b 3 -of GTiff -co TILED=YES -co COMPRESS=JPEG -co JPEG_QUALITY=90 -co alpha=yes -co PHOTOMETRIC=RGB -co BLOCKXSIZE=512 -co BLOCKYSIZE=512 -a_srs EPSG:28992  $src_tif $dst_tif
+    gdal_translate -b 1 -b 2 -b 3 -of GTiff -co TILED=YES -co COMPRESS=JPEG -co JPEG_QUALITY=90 -co alpha=yes -co PHOTOMETRIC=YCBCR -co BLOCKXSIZE=512 -co BLOCKYSIZE=512 -a_srs EPSG:28992  $src_tif $dst_tif
 
     # Maak overview (pyramid)
     echo "Maak overview met gdaladdo"
     # number of pyramids = log(pixelsize of image) / log(2) - log (pixelsize of tile) / log(2).
     # Plaatje is typisch 10000x8000 neem grootste
     # log(10000) / log(2) - log (512) / log(2) = 4.28 = ~5
-    #
+    # 
     #  Now creating pyramids.
     #    gdaladdo %THIS_DIR%.tif -r average --config COMPRESS_OVERVIEW JPEG
     #    --config JPEG_QUALITY_OVERVIEW 60 --config INTERLEAVE_OVERVIEW PIXEL
     #      --config PHOTOMETRIC_OVERVIEW YCBCR 2 4 8 16 32 64 128 256 512
-    gdaladdo -r average $dst_tif  2 4 8 16 32
-
-    # Verplaats doel tif naar doel-dir
+    gdaladdo -r average --config COMPRESS_OVERVIEW JPEG --config PHOTOMETRIC_OVERVIEW YCBCR --config JPEG_QUALITY_OVERVIEW 80 --config INTERLEAVE_OVERVIEW PIXEL $dst_tif  2 4 8 16 32
+    
     mv $dst_tif $dst_dir
     echo "END CONVERT $src_tif"
 
