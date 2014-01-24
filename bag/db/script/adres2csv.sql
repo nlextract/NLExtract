6ac81a5319978b8ad2f37a0804bd3cedc4392be8
Just van den Broecke <just@justobjects.nl>
2014-01-24 21:47:28 +0100

    adres2csv SQL script en versie nr bagextract (was vergeten in v1.1.4)

---
+++ bag/db/script/adres2csv.sql
@@ -0,0 +1,6 @@
+-- Simpel SQL script om adres tabel naar CSV te converteren
+-- bijv met script:
+-- psql -d bag -f adres2csv.sql
+-- mv /tmp/bagadres.csv .
+-- dump is in /tmp omdat postgres absolute pad wil...
+\COPY (SELECT openbareruimtenaam as openbareruimte,huisnummer,huisletter,huisnummertoevoeging,woonplaatsnaam as woonplaats,gemeentenaam as gemeente,provincienaam as provincie, adresseerbaarobject as object_id,typeadresseerbaarobject as object_type,nevenadres,ST_X(geopunt) as x,ST_Y(geopunt) as y, ST_X(ST_Transform(geopunt, 4326)) as lon, ST_Y(ST_Transform(geopunt, 4326)) as lat  FROM adres) TO '/tmp/bagadres.csv' WITH CSV HEADER
