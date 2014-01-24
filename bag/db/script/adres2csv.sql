5642e455ff0ed6d11d95e18758e0e240bff7ca89
Just van den Broecke <just@justobjects.nl>
2014-01-24 22:05:30 +0100

    ORDER BY openbareruimtenaam,huisnummer,huisletter,huisnummertoevoeging

--- bag/db/script/adres2csv.sql
+++ bag/db/script/adres2csv.sql
@@ -4,4 +4,4 @@
 -- mv /tmp/bagadres.csv .
 -- dump is in /tmp omdat postgres absolute pad wil...
 -- voor specifiek schema: verander public.adres naar mijnschema.adres
-\COPY (SELECT openbareruimtenaam as openbareruimte,huisnummer,huisletter,huisnummertoevoeging,woonplaatsnaam as woonplaats,gemeentenaam as gemeente,provincienaam as provincie, adresseerbaarobject as object_id,typeadresseerbaarobject as object_type,nevenadres,ST_X(geopunt) as x,ST_Y(geopunt) as y, ST_X(ST_Transform(geopunt, 4326)) as lon, ST_Y(ST_Transform(geopunt, 4326)) as lat  FROM adres ORDER BY openbareruimtenaam) TO '/tmp/bagadres.csv' WITH CSV HEADER
+\COPY (SELECT openbareruimtenaam as openbareruimte,huisnummer,huisletter,huisnummertoevoeging,woonplaatsnaam as woonplaats,gemeentenaam as gemeente,provincienaam as provincie, adresseerbaarobject as object_id,typeadresseerbaarobject as object_type,nevenadres,ST_X(geopunt) as x,ST_Y(geopunt) as y, ST_X(ST_Transform(geopunt, 4326)) as lon, ST_Y(ST_Transform(geopunt, 4326)) as lat  FROM adres ORDER BY openbareruimtenaam,huisnummer,huisletter,huisnummertoevoeging) TO '/tmp/bagadres.csv' WITH CSV HEADER
