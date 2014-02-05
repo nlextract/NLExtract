4039a2a4e3b0c5cab631f4f5bbc040e0a048c622
Just van den Broecke <just@justobjects.nl>
2014-02-06 00:12:45 +0100

    meer commentaar...voor bonne blad tests

--- bonnebladen/work/blend.sh
+++ bonnebladen/work/blend.sh
@@ -12,9 +12,9 @@
 # 3.2 voor iedere N,W,Z,O: leg aangrenzend blad over origineel zodat alleen de rand "ingeblend" wordt
 
 
+# Maak mask file met alleen de "zwarte randen"
+# bonnemask.png is maskfile die zwarte randen uniform transparant maakt
 function create_maskfile() {
-  # Maak mask file met alleen de "zwarte randen"
-  # bonnemask.png is maskfile die zwarte randen uniform transparant maakt
   echo "create mask file"
   /bin/rm -f bonnemask.png
   convert -colorspace sRGB -size 4019x2527 xc:none -fill '#FFFFFF' \
@@ -26,16 +26,18 @@ function create_maskfile() {
 }
 
 function mask_source_file() {
-  # e.g. map_num is b091-1932
+  # e.g. map_num is b091-1932, resultaat in ${map_num}m.png
   map_num=$1
+
   # Voor iedere kaart maak randen uniform transparant met mask file
   echo "laying bonnemask over $map_num"
   /bin/rm -f rm ${map_num}m.png
   composite -alpha on  -gravity center bonnemask.png ${map_num}.png ${map_num}m.png
 }
 
-# Leg het naburige kaartblad over target, bijvn westkant van blad b092 is overlayed met b091
+# Leg het naburige kaartblad over target, bijv westkant van blad b092 is overlayed met b091
 # De transparante kleuren in de randen worden geblend via 'Multiply'
+# Via "geometry" verschuift de overlap zodat precies de overlappende randen samenvallen
 function create_composite() {
   src=$1
   dst=$2
@@ -44,9 +46,10 @@ function create_composite() {
   /bin/rm -f $dst
 
   echo "create composite: src=$src overlay=$overlay dst=$dst geometry=$geometry"
-  composite -compose Multiply  -geometry $geometry $overlay $src $dst
+  composite -compose Multiply -geometry $geometry $overlay $src $dst
 }
 
+# maak composite uit masked bron blad met omringende N,O,Z,W bladen
 function create_composite_nesw() {
   map_num=$1
   north_num=$2
@@ -68,29 +71,29 @@ function create_composite_nesw() {
   echo "composite west"
   create_composite ${map_num}-result-noz.png ${map_num}-result-nozw.png ${west_num}m.png "-4000-27"
 
-  mv ${map_num}-result-nozw.png ../src-composed/${map_num}.png
+  # mv ${map_num}-result-nozw.png ../src-composed/${map_num}.png
 }
 
+# eenmalig maskfile aanmaken
+create_maskfile
 
-# create_maskfile
-# mask_source_file b057-1932
-# mask_source_file b058-1931
+# maak voor alle bronbladen een masked versie met transparante randen
+mask_source_file b057-1932
+mask_source_file b058-1931
+mask_source_file b073-1932
+mask_source_file b074-1931
+mask_source_file b075-1930
+mask_source_file b076-1930
+mask_source_file b090-1932
+mask_source_file b091-1932
+mask_source_file b092-1928
+mask_source_file b093-1928
+mask_source_file b109-1932
+mask_source_file b110-1928
 
-# mask_source_file b073-1932
-# mask_source_file b074-1931
-# mask_source_file "b075-1930"
-# mask_source_file "b076-1930"
-
-# mask_source_file b090-1932
-# mask_source_file "b091-1932"
-# mask_source_file "b092-1928"
-# mask_source_file "b093-1928"
-
-# mask_source_file "b109-1932"
-# mask_source_file "b110-1928"
-
-# create_composite_nesw b074-1931 b057-1932 b075-1930 b091-1932 b073-1932
-# create_composite_nesw b075-1930 b058-1931 b076-1930 b092-1928 b074-1931
-# create_composite_nesw b091-1932 b074-1931 b092-1928 b109-1932 b090-1932
-# create_composite_nesw b092-1928 b075-1930 b093-1928 b110-1928 b091-1932
+# maak voor ieder blad composite met omringende N,O,Z,W bladen
+create_composite_nesw b074-1931 b057-1932 b075-1930 b091-1932 b073-1932
+create_composite_nesw b075-1930 b058-1931 b076-1930 b092-1928 b074-1931
+create_composite_nesw b091-1932 b074-1931 b092-1928 b109-1932 b090-1932
+create_composite_nesw b092-1928 b075-1930 b093-1928 b110-1928 b091-1932
 
