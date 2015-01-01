d6bcace4f3b441c8c88b7b92164f41bca79d1a73
Frank Steggink <frank@steggink.org>
2015-01-01 23:41:59 +0100

    Eerste versie 3D export BAG/AHN2

---
+++ 3d/bag_ahn2/README.txt
@@ -0,0 +1,74 @@
+Introductie
+-----------
+Met de scripts in deze directory kan data worden geëxporteerd naar verschillende 3D-formaten.
+Een voor de hand liggende combinatie is de BAG en de AHN2
+
+
+Gebruik
+-------
+usage: exportBuildings.py [-h] --format FORMAT --bbox MINX MINY MAXX MAXY
+                          [--pg_host PG_HOST] [--pg_port PG_PORT]
+                          [--pg_db PG_DB] [--pg_schema PG_SCHEMA] --pg_user
+                          PG_USER --pg_password PG_PASS --pg_table PG_TABLE
+                          [--ogr_tsrs OGR_TSRS]
+
+Exporteer gebouwen naar 3D
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --format FORMAT       Outputformaat
+  --bbox MINX MINY MAXX MAXY
+                        Bounding box
+  --pg_host PG_HOST     PostgreSQL server host (default: localhost)
+  --pg_port PG_PORT     PostgreSQL server poort (default: 5432)
+  --pg_db PG_DB         PostgreSQL database (default: bag)
+  --pg_schema PG_SCHEMA
+                        PostgreSQL schema (default: public)
+  --pg_user PG_USER     PostgreSQL gebruikersnaam
+  --pg_password PG_PASS
+                        PostgreSQL wachtwoord
+  --pg_table PG_TABLE   PostgreSQL tabel
+  --ogr_tsrs OGR_TSRS   EPSG-identifer data (default: 28992)
+
+Alle OGR-formaten die 3D ondersteunen, kunnen worden gebruikt, evenals de formaten "Wavefront OBJ" en "CityGML". Het
+Wavefront OBJ-formaat kan worden gebruikt om de gebouwen in 3D-pakketten zoals Maya en Blender te importeren. CityGML is
+toegevoegd, omdat OGR hiervoor geen standaard ondersteuning kent. De gebouwen zullen worden geëxporteerd als prisma's.
+
+De brondata dient in een PostGIS-tabel te staan. De tabel moet tenminste de volgende attributen bevatten:
+* identificatie: unieke identifier van het gebouw
+* geovlak: vlakgeometrie van het gebouw
+* min_height: hoogte van het grondoppervlak
+* avg_height: gemiddelde hoogte van het dakoppervlak
+De BAG-pandentabel van NLExtract voldoet indien hier de kolommen min_height en avg_height aan toegevoegd worden (bijv.
+in een view met standaardwaarden). De hoogtedata is in meters.
+
+Het is mogelijk om de gebouwen te herprojecteren naar een ander coördinatenstelsel. Dit is bijv. nodig voor KML en ook
+om GeoJSON-data te kunnen tonen met Cesium. De hoogtedata zal in meters blijven.
+  
+  
+Voorbeelden
+-----------
+De volgende aanroepen exporteren de gebouwen van het centrum van Amersfoort:
+python exportBuildings.py --format "Wavefront OBJ" --bbox 154750 462700 155750 463700 --pg_user bag --pg_password bag --pg_schema data --pg_table pandmethoogte > c:\temp\output.obj
+python exportBuildings.py --format "CityGML" --bbox 154750 462700 155750 463700 --pg_user bag --pg_password bag --pg_schema data --pg_table pandmethoogte > c:\temp\output.city.gml
+python exportBuildings.py --format "GML" --bbox 154750 462700 155750 463700 --pg_user bag --pg_password bag --pg_schema data --pg_table pandmethoogte --ogr_tsrs 4326 > c:\temp\output.gml
+python exportBuildings.py --format "GeoJSON" --bbox 154750 462700 155750 463700 --pg_user bag --pg_password bag --pg_schema data --pg_table pandmethoogte --ogr_tsrs 4326 > c:\temp\output.json
+
+Let op: de laatste twee worden geherprojecteerd naar WGS84. De eerste twee blijven in RD.
+Deze voorbeelden staan ook in de samples directory
+
+
+Afhankelijkheden
+----------------
+Dit Python-project kent de volgende verplichte afhankelijkheden:
+* psycopg2: Python-binding voor PostgreSQL
+
+Dit Python-project kent de volgende optionele afhankelijkheden, afhankelijk van het outputformaat:
+* Triangle: Triangulatie (alleen Wavefront OBJ)
+* LXML: XML library (alleen CityGML)
+* Osgeo: GDAL/OGR Python bindings (alleen OGR-formaten)
+
+De bindings, m.u.v. triangle, zijn voor Windows hier te vinden: http://www.lfd.uci.edu/~gohlke/pythonlibs/
+
+Triangle is hier te vinden: http://dzhelil.info/triangle/. Easy-install aanroep: python -m easy_install triangle
+Let op: Triangle mag niet worden gebruikt in commerciële applicaties, zonder een licentie van de auteur John Shewchuk. Zie voor meer info: http://www.cs.cmu.edu/~quake/triangle.html
---
+++ 3d/bag_ahn2/src/cityGmlExporter.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python
+#
+# Auteur: Frank Steggink
+# Klassedefinitie voor de CityGmlExporter
+
+import functools
+
+from datetime import datetime
+from lxml import etree
+from threeDExporter import ThreeDExporter
+
+
+# GML / CityGML namespaces
+GML_NS = "http://www.opengis.net/gml"
+GML = "{%s}" % GML_NS
+CGML_NS = "http://www.opengis.net/citygml/1.0"
+CGML = "{%s}" % CGML_NS
+BLDG_NS = "http://www.opengis.net/citygml/building/1.0"
+BLDG = "{%s}" % BLDG_NS
+APP_NS = "http://www.opengis.net/citygml/appearance/1.0"
+APP = "{%s}" % APP_NS
+NSMAP = {None: CGML_NS, "gml": GML_NS, "bldg": BLDG_NS, "app": APP_NS} 
+NSMAP2 = {"gml": GML_NS, "bldg": BLDG_NS, "app": APP_NS} 
+
+
+class CityGmlExporter(ThreeDExporter):
+
+    def __init__(self):
+        self.root = etree.Element(CGML + "CityModel", nsmap=NSMAP)
+
+        self.root.append(etree.Comment("CityGML file gegenereerd d.m.v. NLExtract - bag_ahn2/exportBuildings op " + str(datetime.now()).split(".")[0]))
+        self.root.append(etree.Comment("Gebouwen: o.b.v. BAG"))
+        self.root.append(etree.Comment("Gebouwhoogtes: o.b.v. AHN2"))
+
+        etree.SubElement(self.root, GML + "name").text = "BAG 3D"
+        self.bnd = etree.SubElement(self.root, GML + "boundedBy")
+        
+        self.min_height = float("inf")
+        self.max_height = float("-inf")
+
+
+    # Voegt een gebouw toe aan de exporter
+    def addBuilding(self, id, poly, min_height, avg_height):
+        # Create a building
+        com = etree.SubElement(self.root, CGML + "cityObjectMember")
+        building = etree.SubElement(com, BLDG + "Building")
+        building.set(GML + "id", "g" + str(id))
+        
+        # Add walls: two subsequent coordinates are used to construct a wall face.
+        # A WallSurface is generated for each ring in the geometry.
+        reduce(lambda building, ring: self.boundedBy(building, "WallSurface", map(functools.partial(self.createWallPoly, avg_height, min_height), zip(ring, ring[1:]))), poly, building)
+                
+        # Add roof; after replacing the height
+        roofRings = map(lambda ring: map(lambda c: [c[0], c[1], avg_height], ring), poly)
+        self.boundedBy(building, "RoofSurface", [roofRings])
+        
+        if min_height < self.min_height:
+            self.min_height = min_height
+        if avg_height > self.max_height:
+            self.max_height = avg_height
+        
+
+    # Exporteert de data die de exporter bevat
+    def exportData(self, bbox, crs):
+        self.root.insert(3, etree.Comment("Aantal gebouwen: %d" % self.root.xpath('count(//bldg:Building)', namespaces=NSMAP2)))
+
+        env = etree.SubElement(self.bnd, GML + "Envelope", srsName="EPSG:%d" % crs)
+        etree.SubElement(env, GML + "pos", srsDimension="3").text = "%.3f %.3f %.3f" % (bbox[0], bbox[1], self.min_height)
+        etree.SubElement(env, GML + "pos", srsDimension="3").text = "%.3f %.3f %.3f" % (bbox[2], bbox[3], self.max_height)
+        
+        print(etree.tostring(self.root, pretty_print=True))
+
+
+    # Creates a posList string based on the given coordinates            
+    def posList(self, coords):
+        return reduce(lambda txt, c: txt + " %.3f %.3f %.3f" % (c[0], c[1], c[2] if len(c)>=3 else 0 ), coords, "").strip()
+
+        
+    # Creates a LinearRing geometry, and adds it to the parent
+    def linearRing(self, parent, ring):
+        lrng = etree.SubElement(parent, GML + "LinearRing")
+        plst = etree.SubElement(lrng, GML + "posList", srsDimension="3")  
+        plst.text = self.posList(ring)
+
+        
+    # Creates a Polygon geometry, and adds it to the parent    
+    def polygon(self, parent, rings):
+        pol = etree.SubElement(parent, GML + "Polygon")
+        ext = etree.SubElement(pol, GML + "exterior")
+        self.linearRing(ext, rings[0])
+        
+        for ring in rings[1:]:
+            int = etree.SubElement(pol, GML + "interior")
+            self.linearRing(int, ring)
+        
+
+    # Creates a LoD 2 multisurface geometry, and adds it to the parent   
+    def multiSurface(self, parent, surfaces):
+        msf = etree.SubElement(parent, GML + "MultiSurface")
+        
+        for surface in surfaces:
+            sfm = etree.SubElement(msf, GML + "surfaceMember")
+            #osf = etree.SubElement(sfm, GML + "OrientableSurface", orientation="+")
+            #bsf = etree.SubElement(osf, GML + "baseSurface")
+            self.polygon(sfm, surface)
+
+
+    # Adds a bounding surface to a building    
+    def boundedBy(self, building, surfaceType, surfaces):
+        bnd = etree.SubElement(building, BLDG + "boundedBy")
+        srf = etree.SubElement(bnd, BLDG + surfaceType)
+        lod2msf = etree.SubElement(srf, BLDG + "lod2MultiSurface")
+        self.multiSurface(lod2msf, surfaces)
+        return building
+
+        
+    # Creates a wall polygon constructed from two floor coordinates, the floor height, and the roof height
+    def createWallPoly(self, height_r, height_i, c2):
+        c0 = c2[0]
+        c1 = c2[1]
+        return [[(c0[0],c0[1],height_i),(c1[0],c1[1],height_i),(c1[0],c1[1],height_r),(c0[0],c0[1],height_r),(c0[0],c0[1],height_i)]]
