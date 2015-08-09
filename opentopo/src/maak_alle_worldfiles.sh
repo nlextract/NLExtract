547fc9fd64342d6b2121d9971bf6c234889ec3e2
Just van den Broecke <just@justobjects.nl>
2015-08-09 20:22:53 +0200

    worldfiles 100px/km

--- opentopo/src/maak_alle_worldfiles.sh
+++ opentopo/src/maak_alle_worldfiles.sh
@@ -4,8 +4,8 @@
 
 doc_dir=../doc
 wfiles_dir=../worldfiles
-resos="37.5 150 200 300 400 600 800 1600"
-resos="37.5"
+resos="37.5 100 150 200 300 400 600 800 1600"
+# resos="100"
 
 for reso in $resos; do
    csv_file="${doc_dir}/opentoponl_bladen_${reso}pxkm.csv"
