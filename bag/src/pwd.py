c775f4e4076b3ddd2780c7a7937eed03e7649014
Just van den Broecke <just@justobjects.nl>
2015-10-18 16:09:41 +0200

    remove obsolete pwd.py file

--- bag/src/pwd.py
+++
@@ -1,8 +0,0 @@
-import os,sys
-print "CWD: ",os.getcwd()
-print "Script: ",sys.argv[0]
-print ".EXE: ",os.path.dirname(sys.executable)
-print "Script dir: ", os.path.realpath(os.path.dirname(sys.argv[0]))
-pathname, scriptname = os.path.split(sys.argv[0])
-print "Relative script dir: ",pathname
-print "Script dir: ", os.path.abspath(pathname)
