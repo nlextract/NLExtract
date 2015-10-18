261c1d0f23892ad2b78b9738ddd476b920badaaf
Just van den Broecke <just@justobjects.nl>
2015-10-18 15:38:45 +0200

    issue #147 - make semicolon the delimiter for BAG adres CSV

--- bag/db/script/adres2csv.sql
+++ bag/db/script/adres2csv.sql
@@ -5,6 +5,7 @@
 --    export PGOPTIONS='-c search_path=public,bag8jan2014'
 --    psql -h vm-kademodb -U kademo -W -d bag -f /opt/nlextract/git/bag/db/script/adres2csv.sql
 --    mv /tmp/bagadres.csv bag8jan2014.csv
---
 -- dump is in /tmp omdat postgres absolute pad wil...daar is vast ook wel iets op te bedenken.
-\COPY (SELECT openbareruimtenaam as openbareruimte,huisnummer,huisletter,huisnummertoevoeging,postcode,woonplaatsnaam as woonplaats,gemeentenaam as gemeente,provincienaam as provincie, adresseerbaarobject as object_id,typeadresseerbaarobject as object_type,nevenadres,ST_X(geopunt) as x,ST_Y(geopunt) as y, ST_X(ST_Transform(geopunt, 4326)) as lon, ST_Y(ST_Transform(geopunt, 4326)) as lat  FROM adres ORDER BY postcode,openbareruimtenaam,huisnummer,huisletter,huisnummertoevoeging) TO '/tmp/bagadres.csv' WITH CSV HEADER
+--
+-- 18.okt.2015: forceer puntcomma als CSV veld-delimiter, zie  https://github.com/opengeogroep/NLExtract/issues/147
+\COPY (SELECT openbareruimtenaam as openbareruimte,huisnummer,huisletter,huisnummertoevoeging,postcode,woonplaatsnaam as woonplaats,gemeentenaam as gemeente,provincienaam as provincie, adresseerbaarobject as object_id,typeadresseerbaarobject as object_type,nevenadres,ST_X(geopunt) as x,ST_Y(geopunt) as y, ST_X(ST_Transform(geopunt, 4326)) as lon, ST_Y(ST_Transform(geopunt, 4326)) as lat  FROM adres ORDER BY postcode,openbareruimtenaam,huisnummer,huisletter,huisnummertoevoeging) TO '/tmp/bagadres.csv' WITH CSV HEADER DELIMITER ';'
