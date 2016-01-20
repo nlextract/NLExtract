882c28a86f49f49c15206059060cbcc65bf8c349
Frank Steggink <frank@steggink.org>
2016-01-20 12:45:35 +0100

    Fixed #170: extra check toegevoegd of de te dedupliceren tabel wel bestaat

--- bgt/etl/sql/delete-duplicates-v2.1.1.sql
+++ bgt/etl/sql/delete-duplicates-v2.1.1.sql
@@ -12,14 +12,23 @@ DECLARE
     rowcount INTEGER;
 BEGIN
 
-    EXECUTE 'CREATE TABLE _nlx_temp AS SELECT ogc_fid, lokaalid FROM ' || tablename || ' WHERE lokaalid IN (SELECT lokaalid FROM (SELECT lokaalid, COUNT(*) AS aantal FROM ' || tablename || ' GROUP BY lokaalid ORDER BY lokaalid) AS x WHERE aantal>1);';
+    IF EXISTS (
+        SELECT table_name
+        FROM information_schema.tables
+        WHERE table_schema='{schema}' AND table_name=tablename
+    )
+    THEN
+        EXECUTE 'CREATE TABLE _nlx_temp AS SELECT ogc_fid, lokaalid FROM ' || tablename || ' WHERE lokaalid IN (SELECT lokaalid FROM (SELECT lokaalid, COUNT(*) AS aantal FROM ' || tablename || ' GROUP BY lokaalid ORDER BY lokaalid) AS x WHERE aantal>1);';
 
-    EXECUTE 'DELETE FROM ' || tablename || ' WHERE ogc_fid IN (SELECT ogc_fid FROM _nlx_temp WHERE ogc_fid NOT IN (SELECT MIN(ogc_fid) FROM _nlx_temp GROUP BY lokaalid));';
-    GET DIAGNOSTICS rowcount = ROW_COUNT;
+        EXECUTE 'DELETE FROM ' || tablename || ' WHERE ogc_fid IN (SELECT ogc_fid FROM _nlx_temp WHERE ogc_fid NOT IN (SELECT MIN(ogc_fid) FROM _nlx_temp GROUP BY lokaalid));';
+        GET DIAGNOSTICS rowcount = ROW_COUNT;
 
-    DROP TABLE IF EXISTS _nlx_temp;
+        DROP TABLE IF EXISTS _nlx_temp;
 
-    RETURN tablename || ': ' || rowcount;
+        RETURN tablename || ': ' || rowcount;
+    ELSE
+        RETURN tablename || ' does not exist';
+    END IF;
 
 END;
 $$
