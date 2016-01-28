3b5de47779992276632b592ff4dc61748c5deacb
Frank Steggink <frank@steggink.org>
2016-01-28 16:14:07 +0100

    Jaartal in copyright-notice aangepast

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'NLExtract'
-copyright = u'2012-2015, NLExtract Team'
+copyright = u'2012-2016, NLExtract Team'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
