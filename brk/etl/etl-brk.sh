3e74fdffa06864566bd036b494607b2bd6f4daa4
Just van den Broecke <just@justobjects.nl>
2016-03-27 18:29:28 +0200

    #166 versimpel opties met -a .args files

--- brk/etl/etl-brk.sh
+++ brk/etl/etl-brk.sh
@@ -3,12 +3,12 @@
 # ETL voor BRK GML met gebruik Stetl.
 #
 # Dit is een front-end/wrapper shell-script om uiteindelijk Stetl met een configuratie
-# (etl-brk.cfg) en parameters (in options.sh) aan te roepen.
+# (etl-brk.cfg) en parameters (options/myoptions.args) aan te roepen.
 #
 # Author: Just van den Broecke
 #
 
-. options.sh
+# . options.sh
 
 # Gebruik Stetl meegeleverd met NLExtract (kan in theorie ook Stetl via pip install stetl zijn)
 if [ -z "$STETL_HOME" ]; then
@@ -22,5 +22,18 @@ else
   export PYTHONPATH=$STETL_HOME:$PYTHONPATH
 fi
 
+# Default arguments/options
+options_file=options/default.args
+
+# Optionally overules default options file by using a host-based file options/<your hostname>.args
+# To add your localhost add <your hostname>.args in options directory
+host_options_file=options/`hostname`.args
+
+[ -f "$host_options_file" ] && options_file=$host_options_file
+
+# Evt via commandline overrulen: etl-brk.sh <my options file>
+[ -f "$1" ] && options_file=$1
+
 # Uiteindelijke commando. Kan ook gewoon "stetl -c etl-brk.cfg -a ..." worden indien Stetl installed
-python $STETL_HOME/stetl/main.py -c conf/etl-brk.cfg -a "$pg_options temp_dir=temp max_features=$max_features gml_files=$gml_files $multi $spatial_extent"
+# python $STETL_HOME/stetl/main.py -c conf/etl-brk.cfg -a "$pg_options temp_dir=temp max_features=$max_features gml_files=$gml_files $multi $spatial_extent"
+python $STETL_HOME/stetl/main.py -c conf/etl-brk.cfg -a $options_file
