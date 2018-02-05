59f69bde666d5b320b51a6270a35dc917f4b928b
Frank Steggink <frank@steggink.it>
2018-02-05 16:52:13 +0100

    Rename geometry_xxx columns to geometrie_xxx, so all attribute names are in Dutch

--- top10nl/etl/sql/update-geoms-v1.2.sql
+++ top10nl/etl/sql/update-geoms-v1.2.sql
@@ -1,15 +1,15 @@
 -- Convert polygons to multipolygons in feature types which have both
-update functioneelgebied set geometry_multivlak = st_multi(geometry_vlak), geometry_vlak = null where geometry_vlak is not null;
-alter table functioneelgebied drop column geometry_vlak;
+update functioneelgebied set geometrie_multivlak = st_multi(geometrie_vlak), geometrie_vlak = null where geometrie_vlak is not null;
+alter table functioneelgebied drop column geometrie_vlak;
 
-update geografischgebied set geometry_multivlak = st_multi(geometry_vlak), geometry_vlak = null where geometry_vlak is not null;
-alter table geografischgebied drop column geometry_vlak;
+update geografischgebied set geometrie_multivlak = st_multi(geometrie_vlak), geometrie_vlak = null where geometrie_vlak is not null;
+alter table geografischgebied drop column geometrie_vlak;
 
-update plaats set geometry_multivlak = st_multi(geometry_vlak), geometry_vlak = null where geometry_vlak is not null;
-alter table plaats drop column geometry_vlak;
+update plaats set geometrie_multivlak = st_multi(geometrie_vlak), geometrie_vlak = null where geometrie_vlak is not null;
+alter table plaats drop column geometrie_vlak;
 
-update registratiefgebied set geometry_multivlak = st_multi(geometry_vlak), geometry_vlak = null where geometry_vlak is not null;
-alter table registratiefgebied drop column geometry_vlak;
+update registratiefgebied set geometrie_multivlak = st_multi(geometrie_vlak), geometrie_vlak = null where geometrie_vlak is not null;
+alter table registratiefgebied drop column geometrie_vlak;
 
 -- Rename default column name wkb_geometry to desired name, despite the fact that this has been specified in the GFS file
-alter table terrein rename wkb_geometry to geometry_vlak;
+alter table terrein rename wkb_geometry to geometrie_vlak;
