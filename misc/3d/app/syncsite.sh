6620a10e439272b7af8fbc06e3d80494bda00c1a
Just van den Broecke <just@justobjects.nl>
2018-04-26 15:53:06 +0200

    moved misc dir to new NLEXtract repo: Misc

--- misc/3d/app/syncsite.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-rsync -alzvx -e ssh  . kadmin@kademo:/var/www/nlextract.nl/app/3d
-
-
-
--- misc/bonnebladen/bin/files4periode-all.sh
+++
@@ -1,5 +0,0 @@
-./files4periode.sh 1850 1900
-./files4periode.sh 1901 1925
-./files4periode.sh 1926 1949
-
-
