9477b7a061ddade70fb24afe854d3c545a853c88
Frank Steggink <frank@steggink.it>
2019-07-22 11:19:36 +0200

    Fixed #268: pand_nummeraanduiding is now its own feature class

--- bgt/etl/stetlbgt/pandhandler.py
+++ bgt/etl/stetlbgt/pandhandler.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Filter that deals with OpenbareRuimteLabel objects in BGT GML files.
+# Filter that deals with Pand objects in BGT GML files.
 #
 # Author: Frank Steggink
 
@@ -14,13 +14,13 @@ from stetl.filter import Filter
 from stetl.packet import FORMAT
 from stetl.util import Util
 
-log = Util.get_log("orlhandler")
+log = Util.get_log("pandhandler")
 
 
-class OrlHandler(Filter):
+class PandHandler(Filter):
     """
-    This filter checks whether the data file contains openbareruimtelabels. If this is the case, the positions are split
-    into different features.
+    This filter checks whether the data file contains panden. If this is the case, the buildings and nummeraanduidingen
+    are split into different features.
     """
 
     # Start attribute config meta
@@ -41,7 +41,7 @@ class OrlHandler(Filter):
         Filter.__init__(self, configdict, section, consumes, produces)
 
     def init(self):
-        log.info('Init: OrlHandler')
+        log.info('Init: PandHandler')
         if self.temp_file is None:
             # If no temp_file is present:
             err_s = 'The temp_file needs to be configured'
@@ -49,21 +49,21 @@ class OrlHandler(Filter):
             raise ValueError(err_s)
 
     def exit(self):
-        log.info('Exit: OrlHandler')
+        log.info('Exit: PandHandler')
 
     def invoke(self, packet):
         input_gml = packet.data
         if input_gml is None:
             return packet
 
-        log.info('In OrlHandler.invoke')
+        log.info('In PandHandler.invoke')
 
         if not os.path.exists(input_gml):
             msg = "The given XML file doesn't exist"
             log.error(msg)
             raise ValueError(msg)
 
-        if not self.checkOrlFile(input_gml):
+        if not self.checkPandFile(input_gml):
             return packet
 
         nsmap = {None: "http://www.opengis.net/citygml/2.0"}
@@ -74,8 +74,8 @@ class OrlHandler(Filter):
                     context = etree.iterparse(f)
                     for action, elem in context:
                         if action == 'end' and elem.tag == '{http://www.opengis.net/citygml/2.0}cityObjectMember':
-                            # Duplicate openbareruimtelabel
-                            self.duplicateOrl(xf, elem)
+                            # Duplicate pand
+                            self.duplicatePand(xf, elem)
 
                             # Clean up the original element and the node of its previous sibling
                             # (https://www.ibm.com/developerworks/xml/library/x-hiperfparse/)
@@ -94,15 +94,15 @@ class OrlHandler(Filter):
         # Return the original packet, since this contains the name of the GML file which is being loaded
         return packet
 
-    def checkOrlFile(self, input_gml):
+    def checkPandFile(self, input_gml):
         result = False
         with open(input_gml) as f:
             prevTag = ''
             context = etree.iterparse(f)
             for action, elem in context:
                 if action == 'end' and elem.tag == '{http://www.opengis.net/citygml/2.0}cityObjectMember':
-                    if prevTag == '{http://www.geostandaarden.nl/imgeo/2.1}OpenbareRuimteLabel':
-                        # We're processing a document with openbareruimtelabels, quit
+                    if prevTag == '{http://www.opengis.net/citygml/building/2.0}BuildingPart':
+                        # We're processing a document with panden, quit
                         result = True
 
                     break
@@ -113,19 +113,33 @@ class OrlHandler(Filter):
 
         return result
 
-    def duplicateOrl(self, xf, elem):
+    def duplicatePand(self, xf, elem):
         # The element to be written needs to be deepcopied first, otherwise the counts are off somehow...
         out_elem = deepcopy(elem)
         ns = {'imgeo': 'http://www.geostandaarden.nl/imgeo/2.1'}
-        count = int(out_elem.xpath('count(//imgeo:positie)', namespaces=ns))
+        count = int(out_elem.xpath('count(//imgeo:nummeraanduidingreeks[imgeo:Nummeraanduidingreeks])', namespaces=ns))
 
+        # Write parent Pand object without any Nummeraanduidingreeks
+        out_elem = deepcopy(elem)
+        positions = out_elem.xpath('//imgeo:nummeraanduidingreeks[imgeo:Nummeraanduidingreeks]', namespaces=ns)
+        for pos in positions:
+            pos.getparent().remove(pos)
+        xf.write(out_elem)
+
+        # Write one instance of Nummeraanduidingreeks per Pand
         for i in range(0, count):
             out_elem = deepcopy(elem)
+            out_elem[0].tag = 'BuildingPart_nummeraanduiding'
 
-            # Remove all position elements except for the i'th. This is done by first collecting all positions into
-            # a list, then remove the position from that list which should be kept, and then remove all other positions
-            # from the XML element.
-            positions = out_elem.xpath('//imgeo:positie', namespaces=ns)
+            # Remove the Pand geometry itself
+            buildingSurfaces = out_elem.xpath('//imgeo:geometrie2dGrondvlak', namespaces=ns)
+            for buildingSurface in buildingSurfaces:
+                buildingSurface.getparent().remove(buildingSurface)
+
+            # Remove all nummeraanduiding elements except for the i'th. This is done by first collecting all
+            # nummeraanduidingen into a list, then remove the nummeraanduiding from that list which should be kept,
+            # and then remove all other nummeraanduidingen from the XML element.
+            positions = out_elem.xpath('//imgeo:nummeraanduidingreeks[imgeo:Nummeraanduidingreeks]', namespaces=ns)
             del positions[i]
             for pos in positions:
                 pos.getparent().remove(pos)
