8a8ca43d82b271076923e44ae7a2b1d61b8af05d
Bas Couwenberg <sebastic@xs4all.nl>
2021-04-23 17:26:25 +0200

    Use same provincie_gemeente schema as BAGv1.
    Add gemeentelijke-indeling.py option to convert gemeentelijke-indeling.xml
    to CSV. Use this CSV file to import into PostgreSQL.

--- bagv2/etl/sql/create-tables.sql
+++ bagv2/etl/sql/create-tables.sql
@@ -1,3 +1,5 @@
+CREATE EXTENSION postgis;
+
 -- Tabellen voor BAG-Extract v2
 -- Min of meer 1-1 met BAG model dus niet geoptimaliseerd
 -- Te gebruiken als basis tabellen om specifieke tabellen
@@ -407,17 +409,14 @@ CREATE TABLE gemeente_woonplaats (
 -- Gemeente-Provincie koppeling van CBS
 DROP TABLE IF EXISTS provincie_gemeente CASCADE;
 CREATE TABLE provincie_gemeente (
-  -- follows CBS XLS/CSV column-naming,
+  gid serial,
   provinciecode numeric(4),
-  provinciecodepv character varying(4),
   provincienaam character varying(80),
   gemeentecode character varying(4),
-  gemeentecodegm character varying(80),
   gemeentenaam character varying(80),
   begindatum timestamp with time zone,
   einddatum timestamp with time zone,
-
-  PRIMARY KEY (gemeentecode)
+  PRIMARY KEY (gid)
 );
 
 INSERT INTO nlx_bag_log (actie, bestand) VALUES ('tabellen aangemaakt', 'create-tables.sql');
