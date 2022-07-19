bf4dd64706cebb44156634e44a24d754ff2f2999
Peter Saalbrink <psaalbrink@matrixiangroup.com>
2022-07-19 11:48:34 +0200

    Add SQL queries to process BAG Mutations

---
+++ bagv2/etl/sql/mutations/drop-tables.sql
@@ -0,0 +1,7 @@
+DROP TABLE mutaties_ligplaats;
+DROP TABLE mutaties_nummeraanduiding;
+DROP TABLE mutaties_openbareruimte;
+DROP TABLE mutaties_pand;
+DROP TABLE mutaties_standplaats;
+DROP TABLE mutaties_verblijfsobject;
+DROP TABLE mutaties_woonplaats;
